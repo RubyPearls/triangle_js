3ca74c1837e97d8966e0f94f81cf8562d3357837
jessica <ladyjtm@gmail.com>
2015-03-20 12:20:33 -0700

    Finish debugging commented code

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,11 @@ var triangle = function(length1, length2, length3) {
 
 	if (length1 >= (length2 + length3) || length2 >= (length1 + length3) || length3 >= (length1 + length2)) {
     	
-    	return false;
+    	return "inexistent shape";
     }
 
-	else if(length1 === length2 && length2 === length3) {
+    else if(length1 === length2 && length2 === length3 && length3 === length1) {
+		
 		return "equilateral";
 	}
 
@@ -15,10 +16,29 @@ var triangle = function(length1, length2, length3) {
 		return "isosceles";
 	}
 
-	else if(length1 !== length2 & length2 !== length3 & length1 !== length3) {
+	else if(length1 !== length2 && length2 !== length3 && length1 !== length3) {
 		
 		return "scalene";
 	}
 	
 };
 
+
+
+$(function() {
+  $("form#triangle").submit(function(event) {
+
+     var length1 = parseInt($("#length1").val());
+     var length2 = parseInt($("#length2").val());
+     var length3 = parseInt($("#length3").val());
+
+     var result = triangle(length1, length2, length3);
+
+    $(".tritype").text(result)
+
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
