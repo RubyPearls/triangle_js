d755bdffed1a9efd1ff3fde90f61a4fa02b680cf
jessica <ladyjtm@gmail.com>
2015-03-20 09:54:36 -0700

    Add all specs for desired behaviors

---
+++ spec/specs.js
@@ -0,0 +1,17 @@
+describe("triangle", function() {
+	it("will determine if input is a equilateral triangle", function() {
+		expect(triangle(10, 10, 10)).to.equal(true);
+	});
+
+	it("will determine if input is an isosceles triangle", function() {
+		expect(triangle(15, 10, 15)).to.equal(true);
+	});
+
+	it("will determine if input is a scalene triangle", function() {
+		expect(triangle(12, 10, 15)).to.equal(true);
+	});
+
+	it("will not be a triangle if one side is longer than 2 others combined length", function() {
+		expect(triangle(2, 2, 8)).to.equal(false);
+	});
+});
